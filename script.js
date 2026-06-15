ba1b5f6dc63ce36c84b313e9ea022be541b9e673
tiskoradelight <tiskoradelight@gmail.com>
2026-06-16 01:00:01 +0530

    script.js

--- script.js
+++ script.js
@@ -8,11 +8,34 @@ function saveData() {
 
     let total = amazon + meeshoTD + meeshoMM + flipkart;
 
-    document.getElementById("showDate").innerText = date;
-    document.getElementById("showAmazon").innerText = amazon;
-    document.getElementById("showMeeshoTD").innerText = meeshoTD;
-    document.getElementById("showMeeshoMM").innerText = meeshoMM;
-    document.getElementById("showFlipkart").innerText = flipkart;
-    document.getElementById("showTotal").innerText = total;
+    let orderData = {
+        date: date,
+        amazon: amazon,
+        meeshoTD: meeshoTD,
+        meeshoMM: meeshoMM,
+        flipkart: flipkart,
+        total: total
+    };
 
+    // Data save karna
+    localStorage.setItem("orderData", JSON.stringify(orderData));
+
+    showData();
 }
+
+function showData() {
+
+    let data = JSON.parse(localStorage.getItem("orderData"));
+
+    if (data) {
+        document.getElementById("showDate").innerText = data.date;
+        document.getElementById("showAmazon").innerText = data.amazon;
+        document.getElementById("showMeeshoTD").innerText = data.meeshoTD;
+        document.getElementById("showMeeshoMM").innerText = data.meeshoMM;
+        document.getElementById("showFlipkart").innerText = data.flipkart;
+        document.getElementById("showTotal").innerText = data.total;
+    }
+}
+
+// Page load hote hi data dikhane ke liye
+showData();
