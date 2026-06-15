01f15fe514c1dd340e359bf32d987dbdb07ba3ff
tiskoradelight <tiskoradelight@gmail.com>
2026-06-16 00:55:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Order Dashboard Started");
+function saveData() {
+
+    let date = document.getElementById("date").value;
+    let amazon = Number(document.getElementById("amazon").value);
+    let meeshoTD = Number(document.getElementById("meeshoTD").value);
+    let meeshoMM = Number(document.getElementById("meeshoMM").value);
+    let flipkart = Number(document.getElementById("flipkart").value);
+
+    let total = amazon + meeshoTD + meeshoMM + flipkart;
+
+    document.getElementById("showDate").innerText = date;
+    document.getElementById("showAmazon").innerText = amazon;
+    document.getElementById("showMeeshoTD").innerText = meeshoTD;
+    document.getElementById("showMeeshoMM").innerText = meeshoMM;
+    document.getElementById("showFlipkart").innerText = flipkart;
+    document.getElementById("showTotal").innerText = total;
+
+}
